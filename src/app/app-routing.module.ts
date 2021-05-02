8bb24388aeacc57f045fe6ee1506841bac4a8be1
Davidama <davidarteromartinez@gmail.com>
2021-05-02 19:33:14 +0200

    ser-edit component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
 
 const routes: Routes = [
   {path: '' , component: HomeComponent},
   {path: 'registro' , component: RegisterComponent},
   {path: 'login' , component: LoginComponent},
   {path: 'home' , component: HomeComponent},
+  {path: 'mis-datos' , component: UserEditComponent},
   
 
   
