054cbaf035dca75083e2afec75bb6a4374eb9add
Davidama <davidarteromartinez@gmail.com>
2021-10-11 13:27:25 +0200

    navbar center version

--- src/app/components/profile/profileFriends/profileFriends.component.ts
+++ src/app/components/profile/profileFriends/profileFriends.component.ts
@@ -8,130 +8,110 @@ import { GLOBAL } from 'src/app/services/global';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDialogModule } from '@angular/material/dialog';
 import { dialogElementsUpdateProfilePic } from '../../dialogs/dialogElementsUpdateProfilePic.component';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { PublicationService } from 'src/app/services/publication.service';
 import { Publication } from 'src/app/models/publication';
 import { ProfileComponent } from '../profile.component';
 import * as $ from 'jquery';
 
-
 @Component({
-    selector: 'profileFriends',
-    templateUrl: './profileFriends.component.html',
-    styleUrls: ['./profileFriends.component.scss'],
-    providers: [UserService, FollowService],
-  })
+  selector: 'profileFriends',
+  templateUrl: './profileFriends.component.html',
+  styleUrls: ['./profileFriends.component.scss'],
+  providers: [UserService, FollowService],
+})
+export class ProfileFriendsComponent implements OnInit {
+  public title: string;
+  public user: User;
+  public status: string;
+  public identity;
+  public token;
+  public stats;
+  public url;
+  public follow;
+  public page: number;
+  public total: number;
+  public pages: number;
+  public itemsPerPage: number;
+  public publications: Publication[];
+  public follows;
+  public following;
+  public noMore;
+  public id;
 
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService,
+    private _followService: FollowService,
+    public dialog: MatDialog,
+    private _publicationService: PublicationService
+  ) {
+    this.title = 'Perfil';
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.url = GLOBAL.url;
+    this.page = 1;
+    this.noMore = false;
+  }
 
-  export class ProfileFriendsComponent implements OnInit {
-    public title: string;
-    public user: User;
-    public status: string;
-    public identity;
-    public token;
-    public stats;
-    public url;
-    public follow;
-    public page: number;
-    public total: number;
-    public pages:number;
-    public itemsPerPage:number;
-    public publications: Publication[];
-    public follows;
-    public following;
-    public noMore;
-    public id;
+  ngOnInit(): void {
+    console.log('Profile Publication Component cargado correctamente');
+    this.loadPage();
+  }
 
-    
-    constructor(
+  loadPage() {
+    this._route.params.subscribe((params) => {
+      this.id = params['id'];
+      this.getFollowingUsers(this.id, this.page);
+    });
+  }
 
-        private _route: ActivatedRoute,
-        private _router: Router,
-        private _userService: UserService,
-        private _followService: FollowService,
-        public dialog: MatDialog,
-        private _publicationService: PublicationService
-    ){
-        this.title = 'Perfil';
-        this.identity = this._userService.getIdentity();
-        this.token = this._userService.getToken();
-        this.url = GLOBAL.url;
-        this.page = 1;
-        this.noMore = false;
+  modalUpdateProfile = false;
 
-    }
-    
-    
-    ngOnInit(): void {
-        console.log('Profile Publication Component cargado correctamente');
-        this.loadPage();
+  getFollowingUsers(id, page, adding = false) {
+    this._followService.getFollowing(this.token, id, page).subscribe(
+      (response) => {
+        if (response.follows) {
+          this.total = response.total;
+          this.pages = response.pages;
 
-    }
+          this.itemsPerPage = response.total;
 
-    loadPage(){
-        this._route.params.subscribe(params=>{
-            this.id = params['id'];
-            this.getFollowingUsers(this.id,this.page);
-        })
-    }
+          if (!adding) {
+            //console.log('!adding');
+            this.follows = response.follows;
+            //console.log("publicaciones->",this.publications)
+          } else {
+            var arrayA = this.follows;
+            var arrayB = response.follows;
+            this.follows = arrayA.concat(arrayB);
+            console.log('RESPUESTA FOLLOWS', response);
+          }
+          // alert(response.total);
+          // alert(response.pages);
 
-    modalUpdateProfile = false;
-
-    
-    getFollowingUsers(id,page,adding=false){
-       
-        this._followService.getFollowing(this.token, id, page).subscribe(
-            response=>{
-                if(response.follows){
-
-                        this.total = response.total;
-                        this.pages = response.pages;
-                       
-                        this.itemsPerPage = response.total;
-                        
-
-                        if (!adding) {
-                            //console.log('!adding');
-                            this.follows = response.follows;
-                            //console.log("publicaciones->",this.publications)
-                          }else{
-                            var arrayA = this.follows;
-                            var arrayB = response.follows;
-                            this.follows = arrayA.concat(arrayB);
-                            console.log("RESPUESTA FOLLOWS",response);
-                          }
-                        // alert(response.total);
-                        // alert(response.pages);
-                      
-                        console.log(this.follows)
-                        this.viewMore();
-
-                    }else{
-                    this.status = 'error';
-                    
-                }
-            },
-            error=>{
-                console.log("'/perfil'" + this.identity._id)
-                this._router.navigate(['/perfil', this.identity._id]);
-
-            }
-        )
-
-       
-    }
-
-    viewMore(){            
-        this.page = this.page+1;
-
-        if(this.page <= this.pages){
-          
-          this.getFollowingUsers(this.id,this.page,true);  
-        }else{
-            this.noMore = true;
-            console.log("noMore = true")
+          console.log(this.follows);
+          this.viewMore();
+        } else {
+          this.status = 'error';
         }
-       
-          
+      },
+      (error) => {
+        console.log("'/perfil'" + this.identity._id);
+        this._router.navigate(['/perfil', this.identity._id]);
+      }
+    );
+  }
+
+  viewMore() {
+    this.page = this.page + 1;
+
+    if (this.page <= this.pages) {
+      this.getFollowingUsers(this.id, this.page, true);
+    } else {
+      this.noMore = true;
+      console.log('noMore = true');
     }
   }
+}
