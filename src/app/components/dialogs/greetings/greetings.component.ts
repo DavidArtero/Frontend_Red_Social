849fa0d454dff6ba69f1d0445425895295bff353
Davidama <davidarteromartinez@gmail.com>
2021-10-14 20:48:12 +0200

    new dialog and clean app

---
+++ src/app/components/dialogs/greetings/greetings.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-greetings',
+  templateUrl: './greetings.component.html',
+  styleUrls: ['./greetings.component.scss']
+})
+export class GreetingsComponent implements OnInit {
+
+  constructor(@Inject(MAT_DIALOG_DATA)public data: 
+                  {age:number, name:string},
+                  private matDialogRef:MatDialogRef<GreetingsComponent>) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(){
+    this.matDialogRef.close(this.data);
+  }
+
+  onCloseClick(){
+    this.matDialogRef.close();
+  }
+
+}
